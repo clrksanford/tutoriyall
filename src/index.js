cab218d91c7a59a2ec5024576aa875f0eaf5f655
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-09-16 11:43:56 -0500

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import {render} from 'react-dom';
+import {Router, Route, BrowserRouter} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './routes/App/index.js';
+import Profile from './routes/Profile/index.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={App}/>
-    </Route>
-  </Router>,
+render(
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' component={App}/>
+      <Route path='/profile' component={Profile}/>
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();

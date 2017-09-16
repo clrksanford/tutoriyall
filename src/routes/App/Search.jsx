e6d9be1249d6f8bcb9fb6102d66c1624b3e03ae8
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-09-16 16:05:28 -0500

    Add Seeded data dashboard

---
+++ src/routes/App/Search.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import AutoComplete from 'material-ui/AutoComplete';
+
+export default class Search extends Component {
+  state = {
+    dataSource: [],
+  };
+
+  handleUpdateInput = (value) => {
+    this.setState({
+      dataSource: [
+        value,
+        value + value,
+        value + value + value,
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <AutoComplete
+          hintText="Type anything"
+          dataSource={this.state.dataSource}
+          onUpdateInput={this.handleUpdateInput}
+        />
+      </div>
+    );
+  }
+}

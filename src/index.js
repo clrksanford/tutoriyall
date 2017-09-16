a6080a2a2a8019978040d37c02380d2000d674aa
Clark Sanford <clrksanford@gmail.com>
2017-09-16 10:51:49 -0500

    Stub out React Router routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={App}/>
+    </Route>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
